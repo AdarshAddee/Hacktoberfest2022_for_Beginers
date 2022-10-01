222a8a557ffe8e7824ba41c0fc54403589b05a1e
Adarsh Code <112389532+AdrshCode@users.noreply.github.com>
2022-10-01 10:16:50 +0530

    Create square pattern in c.c

---
+++ C/square pattern in c.c
@@ -0,0 +1,21 @@
+// AdrshCode
+
+#include<stdio.h>
+int main()
+{
+    int x = 0,y = 0;
+    unsigned int squareSide = 0;
+    printf("Enter size of square = ");
+    scanf("%u",&squareSide);
+    //outer loop
+    for(x = 0; x < squareSide; ++x)
+    {
+        //inner loop
+        for(y = 0; y < squareSide; ++y)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
