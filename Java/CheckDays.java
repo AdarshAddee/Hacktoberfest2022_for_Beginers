64cd87cdd48d3b6e78227b90f59c67f5b24e0788
Ahmad Abu Hasan <36786100+eby8zevin@users.noreply.github.com>
2022-10-02 03:34:03 +0700

    Determining the Name of the Day

---
+++ Java/CheckDays.java
@@ -0,0 +1,25 @@
+public class CheckDays {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("# Determining the Name of the Day #");
+        System.out.println("===================================");
+        System.out.println();
+
+        int n;
+
+        System.out.print("Enter the number of days (1-7): ");
+        n = input.nextInt();
+
+        String[] day = {
+                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+        };
+
+        if ((n < 1) || (n > 7)) {
+            System.out.println("Options not available");
+        } else {
+            System.out.println(day[n - 1]);
+        }
+    }
+}
