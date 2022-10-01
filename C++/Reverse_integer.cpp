7b001dbbdfb45c0a43fc45370124d48f40106401
Khushi Marothi <85816852+khushimarothi@users.noreply.github.com>
2022-10-01 12:42:19 +0530

    Create Reverse_integer.cpp

---
+++ C++/Reverse_integer.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/reverse-integer/
+
+//code
+class Solution {
+public:
+    int reverse(int x) {
+        long int reverse=0;
+        while(x!=0){
+            int digit=x%10;
+            reverse=reverse*10+ digit;
+            x=x/10;
+        }
+        return reverse;
+    }
+};
