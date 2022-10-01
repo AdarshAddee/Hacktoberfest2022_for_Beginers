76259afb447494a412bf470606747e05c77d0be7
Prathamesh Patil <91467888+PrathamP31@users.noreply.github.com>
2022-10-02 00:00:52 +0530

    Fibonacci Numbers
    Fibonacci Numbers code in C++ language

---
+++ C++/Fibonacci_numbers.cpp
@@ -0,0 +1,31 @@
+// Fibonacci Numbers 
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55....
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int n;
+        cout << "Enter the value of n : " ;
+        cin >> n;
+        
+        if(n <= 1)
+        {
+                cout << "The nth Fibonacci Number is  : " << n << endl; 
+        }
+        else
+        {
+                int first = 0, second = 1, sum = 0;
+                
+                for(int i = 2; i <= n; i++)
+                {
+                        sum = first + second;
+                        first = second;
+                        second = sum;
+                }
+                
+                cout << "The nth Fibonacci Number is  : " << second << endl; 
+        }
+}
