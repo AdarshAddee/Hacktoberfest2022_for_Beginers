fb54c7e736888604754059cddd4e3e833fce51d2
Rahul Kumar Gupta <67097571+rahulshivan05@users.noreply.github.com>
2022-10-01 15:10:20 +0530

    Create add_numbers.c
    Make a Add two numbers in C programming language.

---
+++ C/add_numbers.c
@@ -0,0 +1,22 @@
+/*
+Rahul Kumar Gupta
+Github = https://github.com/rahulshivan05
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* Adding two numbers */
+
+    float a, b;
+    printf("Enter number a \n");
+    scanf("%d", &a);
+
+    printf("Enter number b\n");
+    scanf("%d", &b);
+
+    printf("The sum of number is %d\n", a + b);
+
+    return 0;
+}
