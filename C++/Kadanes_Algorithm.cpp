20b1413e26a6eb9715707a7666504fa589ca2151
Anant Chaudhary <78264061+anant0070@users.noreply.github.com>
2022-10-02 12:21:50 +0530

    Update Kadanes_Algorithm.cpp
    easy solution

--- C++/Kadanes_Algorithm.cpp
+++ C++/Kadanes_Algorithm.cpp
@@ -56,3 +56,32 @@ int main()
         cout << ob.maxSubarraySum(a, n) << endl;
     }
 }
+
+
+// 2nd method
+
+#include <bits/stdc++.h>
+using namespace std;
+int maxSubArray(int a[],int n){
+    int maxS=INT_MIN;
+    int curS=0;
+
+    for(int i=0;i<n;i++){
+        curS+=a[i];
+    
+        if(curS>maxS){
+            maxS=curS;
+        }
+        if(curS<0){
+            curS=0;
+        }    
+    }
+    return maxS;
+
+}
+
+int main(){
+    int a[]={-1,-2,-3,-4};
+    int n=sizeof(a)/sizeof(a[0]);
+    cout<<maxSubArray(a,n);
+}
