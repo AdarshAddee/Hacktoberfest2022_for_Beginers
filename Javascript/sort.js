6fd57c9e72f6f42e0adf1649e9a34c783a766489
assing <assing.s7644@gmail.com>
2022-10-02 15:35:05 +0700

    Add js sort

---
+++ Javascript/sort.js
@@ -0,0 +1,8 @@
+function sortArray() {
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    fruits.sort();
+    // ['Apple', 'Banana', 'Mango', 'Orange']
+    console.log(fruits)
+}
+
+sortArray();
