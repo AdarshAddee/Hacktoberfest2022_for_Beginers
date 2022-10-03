aac845972866c9f46fb52c7d9a195295e978bbc8
ashutosh sao <63497482+ashutoshsao@users.noreply.github.com>
2022-10-03 19:04:11 +0530

    added while loop file
    a simple way to show how use while loop

---
+++ C++/whileloop.cpp
@@ -0,0 +1,14 @@
+###a simple code example to show how to use while loop
+
+//in this example we are printing a table of 9
+
+#include<iostream>
+using namespace std;
+int main(){
+     int i=1,n=9;
+     while(i<=10){
+            cout<<i<<"*"<<n<<"="<<i*n<<endl;
+            i++;
+     }
+    return 0;
+}
