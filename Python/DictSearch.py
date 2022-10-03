b866116bf70fceab857cffd6826d1454bd4996c1
Vishal Suresh Kesharwani <66909545+vishal10kesharwani@users.noreply.github.com>
2022-10-03 19:08:26 +0530

    Create DictSearch.py

---
+++ Python/DictSearch.py
@@ -0,0 +1,23 @@
+def dic(words):
+
+  wordList = {}
+
+  for index in words:
+
+    try:
+
+      wordList[index] += 1
+
+    except KeyError:
+
+      wordList[index] = 1
+
+  return wordList
+
+ 
+
+wordList='1,3,2,4,5,3,2,1,4,3,2'.split(',')
+
+print wordList
+
+print dic(wordList)
