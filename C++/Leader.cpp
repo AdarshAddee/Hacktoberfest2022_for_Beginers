0fd13ef9826fc9f465b8eee3bcdbffb24da705ab
nikhil730 <79016958+nikhil730@users.noreply.github.com>
2022-10-03 22:56:33 +0530

    Leaders by nikhil730
    Username-nikhil730
    Aim-to find leaders in an array

---
+++ C++/Leader.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+        vector<int>v;
+        int max=a[n-1];
+        v.push_back(a[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(a[i]>=max){
+                v.push_back(a[i]);
+                max=a[i];
+            }
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+  // } Driver Code Ends
