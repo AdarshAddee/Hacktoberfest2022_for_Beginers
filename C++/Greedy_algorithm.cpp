86d27c86040bd4d00837d05853c6520f30d86495
Yash Bansal <73324896+Yash9460@users.noreply.github.com>
2022-10-04 10:33:08 +0530

    Create Greedy_algorithm.cpp

---
+++ C++/Greedy_algorithm.cpp
@@ -0,0 +1,35 @@
+// Github Username - Yash9460
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+int deno[] = { 1, 2, 5, 10, 20, 50, 100, 500, 1000 };
+int n = sizeof(deno) / sizeof(deno[0]);
+ 
+
+void findMin(int V)
+{
+   
+    vector<int> ans;
+ 
+    for (int i = n - 1; i >= 0; i--) {
+      
+        while (V >= deno[i]) {
+            V -= deno[i];
+            ans.push_back(deno[i]);
+        }
+    }
+ 
+  
+    for (int i = 0; i < ans.size(); i++)
+        cout << ans[i] << "  ";
+}
+ 
+
+int main()
+{
+    int n = 93;
+    cout << "Following is minimal number of change for " << n << " is ";
+    findMin(n);
+    return 0;
+}
