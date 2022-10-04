c3a6b3cd4bc6172085605b67c0a9442817015bab
Jillian Oestreicher <jillianoestreicher@Jillians-MacBook-Pro.local>
2022-10-04 19:01:20 -0400

    added program in c

---
+++ C/pattern.c
@@ -0,0 +1,27 @@
+// Github username: jilloestreicher
+// Aim: pyramid pattern
+// Date: Oct. 4th, 2022
+
+#include <stdio.h>  
+int main()  
+{  
+    int n,m;  
+    printf("Enter the number of rows");  
+    scanf("%d",&n);  
+    m=n;  
+   for(int i=1;i<=n;i++)  
+   {  
+       for(int j=1;j<=m-1;j++)  
+       {  
+           printf(" ");  
+       }  
+       for(int k=1;k<=2*i-1;k++)  
+       {  
+         printf("*");  
+       }  
+       m--;  
+     
+      printf("\n");  
+    }  
+    return 0;  
+}  
