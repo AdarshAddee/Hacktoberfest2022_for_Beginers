29298f9d0d6ff62aecfa36e1d4fa5658cf83a7c3
Tamali07 <98543049+Rittika1234@users.noreply.github.com>
2022-10-04 11:59:32 +0530

    Floor Java Using Binary Search

---
+++ Java/Floor.java
@@ -0,0 +1,32 @@
+public class Floor {
+
+	public static void main(String[] args) {
+		int[]arr= {2,3,5,9,14,16,18};
+		int target=8;
+		System.out.println(floor(arr,target));
+		//greatest number smaller or = target
+
+	}
+	static int floor(int[]arr,int target) {
+		if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+	
+		int start=0;
+		int end=arr.length-1;
+		while(start<=end) {
+			int mid=start+(end-start)/2;
+			 if(target>arr[mid]) {
+					start=mid+1;
+				}
+				else if(target<arr[mid]){
+					end=mid-1;
+				}
+				else {
+					return mid;
+				}
+			}
+		 
+			return end;
+	}
+}
