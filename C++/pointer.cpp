df11d5b8866ecac48a342240265d2943a2f63c9e
SharmaRahul321 <94913644+SharmaRahul321@users.noreply.github.com>
2022-10-04 22:55:28 +0530

    Create Smart pointer.cpp

---
+++ C++/pointer.cpp
@@ -0,0 +1,34 @@
+// Github username: SharmaRahul321
+// Aim: Your Repo aim according to your program
+// Date: 4 0ctomber 2022
+
+// start coding
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+private:
+	int length;
+	int breadth;
+};
+
+void fun()
+{
+	/ By taking a pointer p and
+	 dynamically creating object
+	 of class rectangle */
+	Rectangle* p = new Rectangle();
+}
+
+int main()
+{
+	// Infinite Loop
+	while (1) {
+		fun();
+	}
+}
