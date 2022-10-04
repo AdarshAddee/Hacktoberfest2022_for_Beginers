eecd7e5df08dfd00701265e98597706a4a77f914
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-10-04 17:40:06 +0530

    Create Diamond_Shaped_Pattern.py

---
+++ Python/Diamond_Shaped_Pattern.py
@@ -0,0 +1,19 @@
+rows = 5
+k = 2 * rows - 2
+for i in range(0, rows):
+    for j in range(0, k):
+        print(end=" ")
+    k = k - 1
+    for j in range(0, i + 1):
+        print("* ", end="")
+    print("")
+    
+k = rows - 2
+
+for i in range(rows, -1, -1):
+    for j in range(k, 0, -1):
+        print(end=" ")
+    k = k + 1
+    for j in range(0, i + 1):
+        print("* ", end="")
+    print("")
