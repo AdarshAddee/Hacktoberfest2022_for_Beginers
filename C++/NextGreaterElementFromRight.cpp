7094708d6f768c80e625f2b568bb6524ce20b89e
Harshit Sharma <55287441+harshit-3103@users.noreply.github.com>
2022-10-06 00:59:49 +0530

    Next Greater Element From Right

---
+++ C++/NextGreaterElementFromRight.cpp
@@ -0,0 +1,61 @@
+
+
+// Github username: harshit-3103
+// Aim: Find the Next Greater Element from Right in an array.
+// Date: 6 Oct 2022
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Next Greater Right Element
+vector<int> solve(vector<int> a)
+{
+    // output vector
+    vector<int> ans;
+
+    stack<int> s;
+    int n = a.size();
+    // from back side 
+    for(int i=n-1;i>=0;i--){
+        // base case
+        if(s.size()==0){
+            ans.push_back(-1);
+        }
+        else if(s.size()>0 && a[i]<s.top()){
+            ans.push_back(s.top());
+        }
+        else if(s.size()>0 && a[i]>=s.top()){
+            while(s.size()>0 && a[i]>=s.top()){
+                s.pop();
+            }
+            if(s.size()==0){
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(s.top());
+            }
+        }
+        s.push(a[i]);
+    }
+    // reverse the array bcz element store from back side
+    reverse(ans.begin(),ans.end());
+    
+    return ans;
+}
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int> v(n);
+  for(int i=0;i<n;i++){
+      cin>>v[i];
+  }
+  // print the output
+  vector<int> ans = solve(v);
+  for(int i=0;i<n;i++){
+      cout<<ans[i]<<" ";
+  }
+   return 0;
+}
