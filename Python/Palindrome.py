2230606ec38aeb1828980ab875185e55a9062f11
Surabhi Lele <98261167+Girlwithasmile@users.noreply.github.com>
2022-10-05 15:25:29 +0530

    Create Palindrome.py

---
+++ Python/Palindrome.py
@@ -0,0 +1,12 @@
+#Python program to check whether number is Palindrome or not
+num=int(input("Enter a number:"))
+temp=num
+rev=0
+while(num>0):
+    dig=num%10
+    rev=rev*10+dig
+    num=num//10
+if(temp==rev):
+    print("The number is palindrome!")
+else:
+    print("Not a palindrome!")
