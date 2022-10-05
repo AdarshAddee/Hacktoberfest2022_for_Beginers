cfd36e07b3bac763cc99f67a50e40ae6bc06d411
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-05 13:35:49 +0530

    Find factorial through Recursion

---
+++ C++/factorial_recursion.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fact(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fact(n);
+}
