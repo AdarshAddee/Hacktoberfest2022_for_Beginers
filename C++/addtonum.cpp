6ac3e64d3f840cd3774758fde59574bb35c0b803
Vinod Kumar Kuril <kurilvinod9870@gmail.com>
2022-10-06 17:01:06 +0530

    added a c++ code

---
+++ C++/addtonum.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the first number : ";
+    cin>>a;
+    cout<<"Enter the second number : ";
+    cin>>b;
+    cout<<"The addition of "<<a<<" and "<<b<<" = "<<a+b<<endl;
+    return 0;
+}
