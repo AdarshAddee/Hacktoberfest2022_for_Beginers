a95c9e9030d9521878c9d992d3981b435ca132a9
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2022-10-07 17:33:45 +0530

    Update QuickSort.java

--- Java/QuickSort.java
+++ Java/QuickSort.java
@@ -1,51 +1,63 @@
-//Github Username: Udyansingh
-
-import java.util.Arrays;
 import java.util.Scanner;
-
-public class QuickSort {
-
-    public static int partition(int[] arr, int si, int ei) {
-        int pid, piv = arr[si], count = 0, i = si;
-        for (i = si + 1; i <= ei; i++) {
-            if (arr[i] <= piv) {
-                count++;
-            }
-        }
-        pid = count + si;
-        arr[si] = (arr[pid] + arr[si]) - (arr[pid] = arr[si]);
-        while (si < pid && ei > pid) {
-            if (arr[si] < piv) {
-                si++;
-            } else if (arr[ei] > piv) {
-                ei--;
-            } else {
-                arr[si] = (arr[ei] + arr[si]) - (arr[ei--] = arr[si++]);
-            }
-        }
-        return pid;
+// Other imports go here
+// Do NOT change the class name
+class Main{
+    static void printArray(int array[], int size){
+        int i;
+        for (i=0; i < size; i++)
+            System.out.printf("%d\t", array[i]);
+        System.out.printf("\n");
     }
 
-    public static void quickSort(int[] arr, int si, int ei) {
-        if (si < ei) {
-            int p = partition(arr, si, ei);
-            quickSort(arr, si, p - 1);
-            quickSort(arr, p + 1, ei);
+    /* This function takes last element as pivot, and places all smaller
+    elements to left of pivot and all greater elements to right of pivot */
+    static int partition (int array[], int low, int high){
+        int pivot = array[high];    // pivot
+        int i = (low - 1);  // Index of smaller element
+        int j;
+
+        for (j = low; j <= high- 1; j++)
+        {
+            if (array[j] <= pivot)
+            {
+                i++;            // increment index of smaller element
+                // shift all lesser elements in left half
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        // place pivot element at end of smaller elements
+        int temp = array[i+1];
+        array[i+1] = array[high];
+        array[high] = temp;
+        return (i + 1);    // the index of pivot element
+    }
+
+    // Recursive function to sort array with quicksort
+    static void quickSort(int array[], int low, int high){
+        if (low < high){
+            int pivot_index;
+            pivot_index = partition(array, low, high);
+            System.out.printf("Pivot element is %d \n",array[pivot_index]);
+            System.out.printf("Array after pivot partitioning : \n");
+            printArray(array,8);
+
+            // Call quicksort() on left half and right half excluding
+            // pivot element as it is already at proper position i.e.
+            // between lesser and greater elements.
+            quickSort(array, low, pivot_index - 1);
+            quickSort(array, pivot_index + 1, high);
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in).useDelimiter(",");
-        System.out.println("Hint: Enter Array -> 1,2,3,4,5,6 ");
-        System.out.print("Enter Array -> ");
-        String input = sc.nextLine();
-        String[] split = input.split(",");
-        int[] arr = new int[split.length];
-        for (int i = 0; i < split.length; i++) {
-            arr[i] = Integer.parseInt(split[i]);
-        }
-        quickSort(arr, 0, arr.length - 1);
-        System.out.println("Sorted Array -> " + Arrays.toString(arr));
-        sc.close();
+    public static void main(String[] args){
+        int array[] = {17, 13, 20, 16, 19, 24, 22, 21};
+        int n = array.length;
+        System.out.printf("Given array: \n");
+        printArray(array, n);
+        quickSort(array, 0, n-1);
+        System.out.printf("Sorted array: \n");
+        printArray(array, n);
     }
 }
