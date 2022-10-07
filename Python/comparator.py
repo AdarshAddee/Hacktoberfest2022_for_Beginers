fe26f2d2d0f6e4efbc2b3d0192c45db6b0903b21
braj5765 <56026596+braj5765@users.noreply.github.com>
2022-10-08 02:15:07 +0530

    Create comparator.py
    Just to learn basic comparison operation

---
+++ Python/comparator.py
@@ -0,0 +1,11 @@
+// Github username: braj5765
+// Aim: Just to learn basic comparison operation
+// Date: 10/8/2022
+
+// start coding
+a=int(input())
+b=int(input())
+if a>b:
+  print(str(a)+' is greater')
+else:
+  print(str(b)+' is greater')
