fcef1fdc43417cee8eb26647f3b2b7f69e309363
Ishika Sunil <39624382+ishika9@users.noreply.github.com>
2022-10-08 23:53:22 +0530

    Create preorder.cpp

---
+++ C++/preorder.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void helper(TreeNode* root, vector<int> &ans)
+    {
+        if(root==NULL) return;
+         ans.push_back(root->val);
+        helper(root->left,ans);
+        helper(root->right,ans);
+        // return ans;
+    }
+public:
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        helper(root,ans);
+        return ans;
+    }
+};
