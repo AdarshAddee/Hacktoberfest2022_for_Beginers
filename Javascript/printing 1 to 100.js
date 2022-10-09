682cf6af7410d450c4a4133908388938476e32c1
Aditya Rawat <adityarawat2801@gmail.com>
2022-10-09 12:17:09 +0530

    printed 1 to 100 without using number in javascript

---
+++ Javascript/printing 1 to 100.js
@@ -0,0 +1,14 @@
+//  Github username: adityasp1
+// Aim: printing 1 to 100 without using number in javascript
+//  Date: 09th October, 2022
+
+const one = ".".length
+const one1 = ~true
+console.log(one, one1);
+
+
+const ten = "..........".length 
+console.log(ten * ten);
+
+const hundred = "d".charCodeAt(0);
+console.log(hundred);
