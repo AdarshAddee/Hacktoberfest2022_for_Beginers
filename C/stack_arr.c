e730f3c4a7f3bfb4ae9c9242fb918811fdbcbb08
Dev Chouhan <68985102+dev-chouhan@users.noreply.github.com>
2022-10-09 14:58:00 +0530

    Update stack_arr.c
    Better and simply understandable implementation of stack using c.

--- C/stack_arr.c
+++ C/stack_arr.c
@@ -1,45 +1,85 @@
-#include<stdio.h>
-#define MAX 1000
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack{
+    int size;
+    int top;
+    int *S;
+};
+
+void create(struct Stack *st){
+    printf("\nEnter the Size: ");
+    scanf("%d", &st->size);
+    st->top = -1;
+    st->S = (int *)malloc(st->size*sizeof(int));
+
+}
+
+void Display(struct Stack st){
+    printf("\n");
+    for (int i = st.top; i >= 0; i--){
+        printf("%d\t", st.S[i]);
+    }
+    printf("\n");
+}
+
+int isEmpty(struct Stack st){
+    if(st.top == -1)
+        return 1;
+    return 0;
+}
+
+int isFull(struct Stack st){
+    return st.top == st.size-1;
+}
+
+int stackTop(struct Stack st){
+    if (!isEmpty)
+        return st.S[st.top];
+    return -1;
+}
+
+void push(struct Stack *st, int x){
+    if (st->top == st->size-1)
+        printf("\nStack Overflow\n");
+    else{
+        st->top ++;
+        st->S[st->top] = x;
+    }
+}
+
+int pop(struct Stack *st){
+    int x = -1;
+    if (st->top == -1)
+        printf("\nStack Underflow\n");
+    else
+        x = st->S[st->top--];
+    return x;
+}
+
+int peek(struct Stack st, int index){
+    int x = -1;
+    if (st.top-index+1 < 0)
+        printf("\nInvalid index");
+    x = st.S[st.top-index+1];
+
+    return x;
+}
+
 
 int main(){
-	int stack[MAX];
-	int size,ele,ch,top = -1;
-	printf("Enter size of stack : ");
-	scanf("%d",&size);
-	while(1){
-		printf("\n\n0. Exit\n1. Push\n2. Pop\n3. Display\nEnter choice : ");
-		scanf("%d",&ch);
-        if(!ch) break;
-		switch(ch){
-			case 1:
-				printf("Enter element : ");
-				scanf("%d",&ele);
-				if(top < size-1){
-					top++;
-					stack[top] = ele;
-				}
-				else printf("\nStack Overflow!\n");
-				break;
-			case 2:
-				if(top >-1){
-					printf("Popped element : %d",stack[top]);
-					top--;
-				}
-				else printf("\nStack Underflow!\n");
-				break;
-			case 3:
-				if(top>-1){
-					for(ch = top; ch>-1;ch--){
-						if(ch==top) printf("%d <--top\n",stack[ch]);
-						else printf("%d\n",stack[ch]);
-					}
-				}
-				else printf("Empty stack!!!");
-				break;
-			default:
-				printf("\nWrong Choice!!!\n");
-		}
-	}
-	
-	return 0;
+    struct Stack st;
+    create(&st);
+
+    push(&st, 10);
+
+    Display(st);
+
+    push(&st, 69);
+    Display(st);
+    printf("\nThe deleted term is: %d",pop(&st));
+    printf("\nThe deleted term is: %d",pop(&st));
+    printf("\nThe deleted term is: %d",pop(&st));
+    Display(st);
+    return 0;
 }
