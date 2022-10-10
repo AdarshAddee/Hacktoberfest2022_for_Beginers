e1ec8ea5f271249e6aea05c62a74e4c3effcd276
Bipin Kumar Sah <68376500+BipinSah@users.noreply.github.com>
2022-10-10 11:19:18 +0530

    Create butterfly_pattern.cpp

---
+++ C++/butterfly_pattern.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the lines you want to print for the pattern  :  ";
+    cin>>n;
+    cout<<"The output for "<< 2 *n<<" lines is : "<<endl;
+    int j;
+    for(int i=1; i<=n; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            cout<<"*";
+        }
+        int space = 2*n - 2*i;
+        for(j=1 ; j<=space; j++)
+        {
+            cout<<" ";
+        }
+        for(j=1; j<=i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=n; i>=1; i--)
+    {
+        for(j=1; j<=i; j++)
+        {
+            cout<<"*";
+        }
+        int space = 2*n - 2*i;
+        for(j=1 ; j<=space; j++)
+        {
+            cout<<" ";
+        }
+        for(j=1; j<=i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+// Github username: BipinSah
+// Aim: To create a butterfly pattern program
+// Date: 10th of October,2022
