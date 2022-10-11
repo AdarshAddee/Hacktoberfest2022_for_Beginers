f3c7658934b8d003ad17cd4e70f1bca24b28e0ea
nickel-28 <nikhilraj875727@gmail.com>
2022-10-11 11:38:55 +0530

    Added LCS problem of DP

---
+++ C++/LargestCommonSubsequence.cpp
@@ -0,0 +1,29 @@
+// Github username: nickel-28
+// Aim: C++ implementation of Largest Common Subsequence
+// Date: Date of Coding
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lcs( char *X, char *Y, int m, int n )
+{
+	if (m == 0 || n == 0)
+		return 0;
+	if (X[m-1] == Y[n-1])
+		return 1 + lcs(X, Y, m-1, n-1);
+	else
+		return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
+}
+
+int main()
+{
+	char X[] = "AGGTAB";
+	char Y[] = "GXTXAYB";
+	
+	int m = strlen(X);
+	int n = strlen(Y);
+	
+	cout<<"Length of LCS is "<< lcs( X, Y, m, n ) ;
+	
+	return 0;
+}
