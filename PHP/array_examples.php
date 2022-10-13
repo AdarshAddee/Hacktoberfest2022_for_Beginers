2f0b97f3282d6e8fd8db112cae2c4cfbdc962969
anugrah rachman <mac@macs-MacBook-Air.local>
2022-10-13 14:46:15 +0700

    added my code

---
+++ PHP/array_examples.php
@@ -0,0 +1,9 @@
+<!-- 
+Github username: kid-omega
+Aim: Hacktoberfest2022_for_Beginers/PHP/array_examples.php
+Date: October 13th 2022 -->
+
+<?php
+$cars=array("Volvo","BMW","Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
