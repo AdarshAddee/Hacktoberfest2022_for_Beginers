5d12abcb3c3ecc06fe7b1417f27b354ebdceb9da
hellovaibhav <82583704+hellovaibhav@users.noreply.github.com>
2022-10-15 16:05:42 +0530

    Tower of Hanoi extended .cpp
    increased the capability to take larger input of number and made the code more readable

--- C++/Tower of Hanoi By Suraj.cpp
+++ C++/Tower of Hanoi By Suraj.cpp
@@ -1,27 +1,31 @@
-    #include<iostream>
-    #include<conio.h>
-    using namespace std;
-    void TOH(int d, char t1, char t2, char t3)
+// header file
+#include <iostream>
+
+using namespace std;
+
+// recursive function to do the sorting
+void TowerOfHanoi(unsigned long long n, char from_rod, char to_rod,
+                  char temp_rod)
+{
+    if (n == 0)
     {
-    if(d==1)
-    {
-    cout<<"\nShift top disk from tower"<<t1<<" to tower"<<t2;
-    return;
+        return;
     }
-    TOH(d-1,t1,t3,t2);
-    cout<<"\nShift top disk from tower"<<t1<<" to tower"<<t2;
-    TOH(d-1,t3,t2,t1);
-    }
-    int main()
-    {
-    int disk;
-    cout<<"Enter the number of disks:"; cin>>disk;
-    if(disk<1)
-    cout<<"There are no disks to shift";
-    else
-    cout<<"There are "<<disk<<"disks in tower 1\n";
-    TOH(disk, '1','2','3');
-    cout<<"\n\n"<<disk<<"disks in tower 1 are shifted to tower 2";
-    getch();
+    TowerOfHanoi(n - 1, from_rod, temp_rod, to_rod);
+    cout << "Moved disk " << n << " from rod " << from_rod
+         << " to rod " << to_rod << endl;
+    TowerOfHanoi(n - 1, temp_rod, to_rod, from_rod);
+}
+
+int main()
+{
+    // changed int to long long to increase the value intakes
+    unsigned long long N;
+
+    cout << "Enter the number of Disks : ";
+
+    cin >> N;
+    // A, B and C are names of rods A=first B=second C=third
+    TowerOfHanoi(N, 'A', 'C', 'B');
     return 0;
-    }
+}
