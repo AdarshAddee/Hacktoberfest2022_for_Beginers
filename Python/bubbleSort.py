07b5444a6335b2a7a70e3db1815600d84d3dff39
Yukta Goel <78146025+yuktagoel@users.noreply.github.com>
2022-10-18 10:58:51 +0530

    Create bubbleSort.py

---
+++ Python/bubbleSort.py
@@ -0,0 +1,19 @@
+def bubbleSort(arr):
+	n = len(arr)
+	swapped = False
+	for i in range(n-1):
+		for j in range(0, n-i-1):
+			if arr[j] > arr[j + 1]:
+				swapped = True
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+		if not swapped:
+			return
+
+
+arr = [4,3,2,6,1]
+
+bubbleSort(arr)
+
+print("Sorted array is:")
+for i in range(len(arr)):
+	print("% d" % arr[i], end=" ")
