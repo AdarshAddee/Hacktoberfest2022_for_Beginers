8fefc95f5dca189e454a16a142ccff48a8ce37de
Sayandip08 <76720567+Sayandip08@users.noreply.github.com>
2022-10-19 22:55:48 +0530

    Create TwoSum.py
    Two Sum in Python

---
+++ Python/TwoSum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+   def twoSum(self, nums, target):
+      """
+      :type nums: List[int]
+      :type target: int
+      :rtype: List[int]
+      """
+      required = {}
+      for i in range(len(nums)):
+         if target - nums[i] in required:
+            return [required[target - nums[i]],i]
+         else:
+            required[nums[i]]=i
+input_list = [2,8,12,15]
+ob1 = Solution()
+print(ob1.twoSum(input_list, 20))
