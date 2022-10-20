05e176c08f62d52d7bedc5257015f40efca4e1ab
garvitanand1 <110623623+garvitanand1@users.noreply.github.com>
2022-10-20 11:07:18 +0530

    Create garvutgt.cpp

---
+++ garvutgt.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sum;
+    
+  cout << "Enter two integers: ";
+  cin >> first_number >> second_number;
+
+  
+  sum = first_number + second_number;
+
+  cout << first_number << " + " <<  second_number << " = " << sum;     
+
+  return 0;
+}
