86d5e3d2776915d48164571d6e27607ba16291ee
sohail9972 <2018ece_sohailmaidargi@nie.ac.in>
2022-10-21 00:43:07 +0530

    first come First Serve Scheduling Algorithm

---
+++ C++/FCFS.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{   int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j;
+    cout<<"Enter total number of processes(maximum 20):";
+    cin>>n;
+ 
+    cout<<"\nEnter Process Burst Time aka DURATION \n";
+    for(i=0;i<n;i++)
+    {
+        cout<<"P["<<i+1<<"]:";
+        cin>>bt[i];
+    }
+    wt[0]=0;    
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+    }
+    cout<<"\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time";
+    
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];
+        avwt+=wt[i];
+        avtat+=tat[i];
+        cout<<"\nP["<<i+1<<"]"<<"\t\t"<<bt[i]<<"\t\t"<<wt[i]<<"\t\t"<<tat[i];
+    }
+    avwt/=i;
+    avtat/=i;
+    cout<<"\n\nAverage Waiting Time:"<<avwt;
+    cout<<"\nAverage Turnaround Time:"<<avtat;
+ 
+    return 0;
+}
