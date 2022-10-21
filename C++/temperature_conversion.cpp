0eaf8946f55ec18ac49e0f08acbca026469e4a25
Deepankar <rekhamandal5280@gmail.com>
2022-10-21 15:16:01 +0530

    added temperature conversion from Celsius to Fahrenheit using c++ functions

---
+++ C++/temperature_conversion.cpp
@@ -0,0 +1,37 @@
+// Temperature Conversion Celsius To Fahrenheit And Vice Versa.
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int celsi(long a){
+    long t;
+    t=(a - 32.0) * 5.0 / 9.0;
+    return t;
+}
+int fahren(long a){
+    long t;
+    t=(a*9/5)+32;
+    return t;
+}
+
+int main(){
+    long temp;
+    cout<<"enter the temp:";
+    cin>>temp;
+    int l;
+    cout<<"type 1 to convert to celsius"<<endl;
+    cout<<"type 2 to convert to fahrenheit"<<endl;
+    cin>>l;
+    switch(l){
+        case 1:
+        cout<<setprecision(6)<<celsi(temp);
+        break;
+        case 2:
+        cout<<setprecision(6)<<fahren(temp);
+        break;
+        default:
+        cout<<"enter proper choice";
+        break;
+    }
+    return 0;
+}
