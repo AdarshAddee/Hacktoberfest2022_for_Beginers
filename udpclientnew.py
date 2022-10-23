556943a6419202cc3c6f3f5bd7ff1df7133ac70c
Advaith Nair <77977360+RazerAds@users.noreply.github.com>
2022-10-23 21:41:11 +0530

    Delete udpclientnew.py

--- udpclientnew.py
+++
@@ -1,9 +0,0 @@
-from socket import*
-serv_addr= "192.168.161.129"
-serv_port=7000
-client_sock=socket(AF_INET,SOCK_DGRAM)
-while 1:
-	msg=input("Enter the text message: ")
-	client_sock.sendto(msg.encode(),(serv_addr,serv_port))
-	mod_msg,s=client_sock.recvfrom(2048)
-	print("From Server:",mod_msg.decode())
