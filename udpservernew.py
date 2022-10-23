5a694b65def9614a623d59cb970ecc1fdfd95d85
Advaith Nair <77977360+RazerAds@users.noreply.github.com>
2022-10-23 21:39:31 +0530

    Add files via upload

---
+++ udpservernew.py
@@ -0,0 +1,11 @@
+from socket import*
+serv_addr= "192.168.161.129"
+serv_port=7000
+serv_sock=socket(AF_INET,SOCK_DGRAM)
+serv_sock.bind((serv_addr,serv_port))
+print(("The server is ready to receive"))
+while 1:
+	msg,client_addr = serv_sock.recvfrom(2048)
+	print("Got message from",client_addr,"\n Message: ",msg)
+	mod_msg=msg.upper()
+	serv_sock.sendto(mod_msg,client_addr)
