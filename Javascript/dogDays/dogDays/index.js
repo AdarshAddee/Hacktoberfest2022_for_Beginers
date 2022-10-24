411e3fc62dd6733fbc8e39e734334f45e91aec38
Yinka <akinn.yinka@gmail.com>
2022-10-24 04:56:33 +0100

    Completed tasks

---
+++ Javascript/dogDays/dogDays/index.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    let dogName = prompt('What is your dog\'s name?');
+
+    let dogAge = prompt('How old is ' + dogName + ' ?');
+
+    let humanAge = (dogAge - 2) * 4 + 21;
+         alert(dogName + ' is ' + humanAge + ' in human years!')
+  }
+  
+  // Github username: Tahree22. 
+  // Aim: This repo demonstrates a simple use of variables in javascript and the box model in CSS. 
+  // Date: October 24, 2022.
