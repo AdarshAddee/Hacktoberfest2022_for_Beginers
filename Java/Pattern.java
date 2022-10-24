8965c6a3c22a101937ff384c047dd9d18e9c99c2
SNEHA <89248226+Sneha2319@users.noreply.github.com>
2022-10-24 11:45:32 +0530

    Create Pattern.java

---
+++ Java/Pattern.java
@@ -0,0 +1,23 @@
+public class Main
+{
+  public static void main(String[] args){
+   for(int i=1; i<=4;i++){
+        for(int j=1; j<=i;j++){
+            System.out.print("*");
+        }System.out.println();                     
+    }
+    for(int i=1; i<=3;i++){
+        for(int j=3;j>=i;j--){
+            System.out.print("*");
+        }System.out.println();
+    }
+}}
+
+//Output
+// *
+// **
+// ***
+// ****
+// ***
+// **
+// *
