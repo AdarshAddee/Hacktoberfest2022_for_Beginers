b534796beff0d469112b755e0b61c3ff570c3795
Sannidhya Sahu <sannidhyasahu915@gmail.com>
2022-10-26 01:32:22 +0530

    Added Addition.cpp

---
+++ C++/Addition.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+    cout<<"a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+    cout<<a<<" + "<<b<<" = "<<a+b;
+    return 0;
+}
