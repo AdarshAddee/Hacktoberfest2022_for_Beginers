4bfec5ac582c7f7d17223c209b15493622fe8194
unknown <kskkorada76@gmail.com>
2022-10-27 22:12:00 +0530

    added java pattern program

---
+++ Java/half_pyramid_pattern.java
@@ -0,0 +1,10 @@
+public class half_pyramid_pattern{
+    public static void main(String args []){
+        for(int i =1; i<=4; i++){
+            for(int j =1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
