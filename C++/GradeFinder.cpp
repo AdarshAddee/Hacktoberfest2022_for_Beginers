b57c0b0bea62ec67accd75a1839184f58e722e9c
Hiza Tariq <bsef19m505@pucit.edu.pk>
2022-10-29 17:34:08 +0500

    Contributed a Cpp Algorithm

---
+++ C++/GradeFinder.cpp
@@ -0,0 +1,41 @@
+//GithubUserName=BSEF19M505
+//Aim=Contributing with this grade finding algorithm in open source in hacktoberfest2022  
+//date=29 oct,2022
+#include <iostream>
+using namespace std;
+int main(){
+    int obtainedmarks;
+    cout<<"Enter the obtainedmarks: ";
+    cin>>obtainedmarks;
+    if (obtainedmarks >= 85){
+        cout<<"Your Grade is A";
+    }
+    else if (obtainedmarks >= 80){
+        cout<<"Your Grade is A-";
+    }
+    else if (obtainedmarks >= 75){
+        cout<<"Your Grade is B+";
+    }
+    else if (obtainedmarks >= 70){
+        cout<<"Your Grade is B";
+    }
+    else if (obtainedmarks >= 65){
+        cout<<"Your Grade is B-";
+    }
+    else if (obtainedmarks >= 60){
+        cout<<"Your Grade is C+";
+    }
+    else if (obtainedmarks >= 55){
+        cout<<"Your Grade is C";
+    }
+    else if (obtainedmarks >= 50){
+        cout<<"Your Grade is D";
+    }
+	else if (obtainedmarks < 50){
+        cout<<"Your Grade is F";
+    }
+    else{
+        cout<<"Enter a valid number";
+    }
+    return 0;
+}
