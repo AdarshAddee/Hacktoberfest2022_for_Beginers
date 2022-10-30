57d395377b62006310a463c618df327e700e55d8
MadNMaxx <57186119+madnmaxx@users.noreply.github.com>
2022-10-30 10:02:18 +0530

    Create queue.js

---
+++ Javascript/queue.js
@@ -0,0 +1,66 @@
+// Github username: madnmaxx
+// Aim: To share data structure code in js.
+// Date: 30-10-22
+
+
+
+
+
+// program to implement queue data structure
+
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+    
+    // add element to the queue
+    enqueue(element) {
+        return this.items.push(element);
+    }
+    
+    // remove element from the queue
+    dequeue() {
+        if(this.items.length > 0) {
+            return this.items.shift();
+        }
+    }
+    
+    // view the last element
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+    
+    // check if the queue is empty
+    isEmpty(){
+       return this.items.length == 0;
+    }
+   
+    // the size of the queue
+    size(){
+        return this.items.length;
+    }
+ 
+    // empty the queue
+    clear(){
+        this.items = [];
+    }
+}
+
+let queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(4);
+queue.enqueue(8);
+console.log(queue.items);
+
+queue.dequeue();
+console.log(queue.items);
+
+console.log(queue.peek());
+
+console.log(queue.isEmpty());
+
+console.log(queue.size());
+
+queue.clear();
+console.log(queue.items);
